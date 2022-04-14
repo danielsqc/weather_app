75494f6ff6c44059c3d5f2d5decfb8d9abec45cc
Paulo <pmendes.ti@gmail.com>
2022-04-14 20:22:26 -0300

    Refatorando testes

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -4,19 +4,17 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-
 export class WeatherService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient: HttpClient) { }
-
-  // private apiKey = 'fd8189e62f5b5ba6f7e7006d89d68373';
   private apiKey = '7717925a5557b50ed7ab69a87e2f8ad3';
   
   getCelsius(temperature: number) {
-    return parseFloat((temperature - 273.15).toFixed(1))
+    return parseFloat((temperature - 273.15).toFixed(1));
+  }
 
-  }
   getCurrentWeather() {
-    return this.httpClient.get(`https://api.openweathermap.org/data/2.5/weather?lat=43.6532&lon=-79.3832&appid=${this.apiKey}`)
+    return this.httpClient.get(`https://api.openweathermap.org/data/2.5/weather?lat=43.6532&lon=-79.3832&appid=${this.apiKey}`);
   }
+  
 }
